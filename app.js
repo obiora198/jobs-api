f4f5b8265ba914aeb48bb5690398f9f1e4fb7089
obiora emmanuel <emmanuelobiora11@gmail.com>
2024-03-06 22:22:21 +0100

    add swagger documentation

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -26,13 +30,6 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-app.get("/", (req, res) => {
-  res.send("jobs api");
-});
-// routes
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", authenticateUser, jobsRouter);
-
 app.set("trust proxy", 1);
 app.use(
   rateLimiter({
@@ -40,6 +37,17 @@ app.use(
     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
   })
 );
+
+
+app.get("/", (req, res) => {
+  res.send("<h1>Jobs Api</h1><a href='/api-docs'>see documentation</a>");
+});
+
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocument))
+
+// routes
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
